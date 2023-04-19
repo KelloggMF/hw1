266ed9f949bf8386c8695d0eafe1179cc04a447b
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-04-19 02:06:49 +0000

    Added code for studios + roles tables

--- kmdb.sql
+++ kmdb.sql
@@ -1,6 +1,7 @@
 
 -- Dropping tables (if they exist, of course!)
 DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS studios;
 
 -- Creating and populating films table 
 CREATE TABLE films (
@@ -50,4 +51,216 @@ VALUES (
     "Warner Bros."
 );
 
+-- Creating and populating studios table
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    studio_name TEXT,
+    film_title TEXT
+);
 
+INSERT INTO studios (
+    studio_name,
+    film_title
+    )
+VALUES (
+    "Warner Bros.",
+    "Batman Begins"
+);
+
+INSERT INTO studios (
+    studio_name,
+    film_title
+    )
+VALUES (
+    "Warner Bros.",
+    "The Dark Knight"
+);
+
+INSERT INTO studios (
+    studio_name,
+    film_title
+    )
+VALUES (
+    "Warner Bros.",
+    "The Dark Knight Rises"
+);
+
+-- Creating and populating roles table
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    film_title TEXT,
+    actor_name TEXT,
+    role_name TEXT
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "Batman Begins",
+    "Christian Bale",
+    "Bruce Wayne"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "Batman Begins",
+    "Michael Caine",
+    "Alfred"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "Batman Begins",
+    "Liam Neeson",
+    "Ra's Al Ghul"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "Batman Begins",
+    "Katie Holmes",
+    "Rachel Dawes"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "Batman Begins",
+    "Gary Oldman",
+    "Commissioner Gordon"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight",
+    "Christian Bale",
+    "Bruce Wayne"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight",
+    "Heath Ledger",
+    "Joker"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight",
+    "Aaron Eckhart",
+    "Harvey Dent"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight",
+    "Michael Caine",
+    "Alfred"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight",
+    "Maggie Gyllenhaal",
+    "Rachel Dawes"
+);
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Christian Bale",
+    "Bruce Wayne"
+);
+
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Gary Oldman",
+    "Commissioner Gordon"
+);
+
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Tom Hardy",
+    "Bane"
+);
+
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Joseph Gordon-Levitt",
+    "John Blake"
+);
+
+
+INSERT INTO roles (
+    film_title,
+    actor_name,
+    role_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Anne Hathaway",
+    "Selina Kyle"
+);
+
+-- Realizing that I'm not using foreign key nomenclature here (e.g., film_id in the roles section).
+-- I'm choosing not to do this because I imagine a user would rather see the film's title, rather than the corresponding ID number.
